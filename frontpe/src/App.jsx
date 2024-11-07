e34371c4c7197638a0095f695f7480af2cd8deba
bayronj <baironjojoa@gmail.com>
2024-11-06 19:30:15 -0500

    front

---
+++ frontpe/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Register from './register';
+import Login from './login';
+
+function App() {
+    return (
+        <div>
+            <h1>Bienvenido a la Tienda en Línea</h1>
+            <Login />
+        </div>
+    );
+}
+
+export default App;
