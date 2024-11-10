c44a892bb0c5395dc92a6942fe89550494f5ea05
bayronj <baironjojoa@gmail.com>
2024-11-10 12:20:36 -0500

    error

--- ecommerce_backend/accounts/serializers.py
+++ ecommerce_backend/accounts/serializers.py
@@ -37,7 +37,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'password', 'phone_number')
+        fields = ('id', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
