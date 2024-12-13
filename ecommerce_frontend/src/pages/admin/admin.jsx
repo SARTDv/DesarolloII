8d7c6ea0e56a534d2dc60adb13099ead75edc46d
SARTDv <munozalejandro531@gmail.com>
2024-12-13 01:41:10 -0500

    Modificacion comentarios de admin.jsx

--- ecommerce_frontend/src/pages/admin/admin.jsx
+++ ecommerce_frontend/src/pages/admin/admin.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { AdminDashboard } from './AdminDashboard';
 import styles from '../../css/admin.module.css';
+
 function Admin() {
+  // Usar la misma función de isSuperuser para determinar si se debe mostrar el dashboard
+  const isSuperuser = true;
+
   return (
-    <div className={styles.admin}>
-      <div className={`${styles["min-h-screen"]} ${styles["bg-gray-100"]}`} >
-        <AdminDashboard />
+    isSuperuser && (
+      <div className={styles.admin}>
+        <div className={`${styles["min-h-screen"]} ${styles["bg-gray-100"]}`}>
+          <AdminDashboard />
+        </div>
       </div>
-    </div>
-    
+    )
   );
 }
 
