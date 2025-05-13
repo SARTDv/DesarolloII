be0c854486ae3db7339c2a07df06deb833575a16
SartDv <munozalejandro531@gmail.com>
2025-05-13 05:57:31 -0500

    pruebasd depuracion

--- ecommerce_backend/ecommerce_backend/wsgi.py
+++ ecommerce_backend/ecommerce_backend/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_backend.settings')
 
+def print_env_vars():
+    for key, value in os.environ.items():
+        print(f"{key} = {value}")
+print("=== VARIABLES DE ENTORNO CARGADAS ===")
+print_env_vars()
+print("=====================================")
+
 application = get_wsgi_application()
